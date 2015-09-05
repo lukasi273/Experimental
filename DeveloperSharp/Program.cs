d7ed136843fba3a6340f9f75402e22bdd0cddac4
lukasi273 <lukasani2273@gmail.com>
2015-09-05 23:12:16 +0200

    Update Program.cs

--- DeveloperSharp/Program.cs
+++ DeveloperSharp/Program.cs
@@ -1,23 +1,25 @@
-﻿using System;
+#region
+
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
-using Color = System.Drawing.Color;
-using SharpDX;
+
+#endregion
 
 namespace DeveloperSharp
 {
-    class Program
+    internal class Program
     {
-        private static List<GameObject> _lstGameObjects = new List<GameObject>(); 
+        private static List<GameObject> _lstGameObjects = new List<GameObject>();
         private static List<GameObject> _lstObjCloseToMouse = new List<GameObject>();
-        private static Menu Config;
-        private static int _lastUpdateTick = 0;
-        private static int _lastMovementTick = 0;
-        static void Main(string[] args)
+        private static Menu _config;
+        private static int _lastUpdateTick;
+        private static int _lastMovementTick;
+
+        private static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += sD =>
             {
@@ -29,9 +31,9 @@ namespace DeveloperSharp
 
         private static void InitMenu()
         {
-            Config = new Menu("Developer#", "developersharp", true);
-            Config.AddItem(new MenuItem("range", "Max object dist from cursor").SetValue(new Slider(400, 100, 1000)));
-            Config.AddToMainMenu();
+            _config = new Menu("Developer#", "developersharp", true);
+            _config.AddItem(new MenuItem("range", "Max object dist from cursor").SetValue(new Slider(400, 100, 1000)));
+            _config.AddToMainMenu();
         }
 
         private static void OnUpdate(EventArgs args)
@@ -40,13 +42,19 @@ namespace DeveloperSharp
             {
                 _lstGameObjects = ObjectManager.Get<GameObject>().ToList();
                 _lstObjCloseToMouse =
-                    _lstGameObjects.Where(o => o.Position.Distance(Game.CursorPos) < Config.Item("range").GetValue<Slider>().Value && !(o is Obj_Turret) && o.Name != "missile" && !(o is Obj_LampBulb) && !(o is Obj_SpellMissile) && !(o is GrassObject) && !(o is DrawFX) && !(o is LevelPropSpawnerPoint) && !(o is Obj_GeneralParticleEmitter) && !o.Name.Contains("MoveTo")).ToList();
+                    _lstGameObjects.Where(
+                        o =>
+                            o.Position.Distance(Game.CursorPos) < _config.Item("range").GetValue<Slider>().Value &&
+                            !(o is Obj_Turret) && o.Name != "missile" && !(o is Obj_LampBulb) &&
+                            !(o is Obj_SpellMissile) && !(o is MissileClient) && !(o is GrassObject) && !(o is DrawFX) &&
+                            !(o is LevelPropSpawnerPoint) && !(o is Obj_GeneralParticleEmitter) &&
+                            !o.Name.Contains("MoveTo")).ToList();
                 _lastUpdateTick = Environment.TickCount;
             }
             if (Environment.TickCount - _lastMovementTick > 140000)
             {
-                ObjectManager.Player.IssueOrder(GameObjectOrder.MoveTo,
-                    ObjectManager.Player.Position.Randomize(-1000, 1000));
+                ObjectManager.Player.IssueOrder(
+                    GameObjectOrder.MoveTo, ObjectManager.Player.Position.Randomize(-1000, 1000));
                 _lastMovementTick = Environment.TickCount;
             }
         }
@@ -55,51 +63,29 @@ namespace DeveloperSharp
         {
             foreach (var obj in _lstObjCloseToMouse)
             {
-                if (!obj.IsValid) return;
-                var X = Drawing.WorldToScreen(obj.Position).X;
-                var Y = Drawing.WorldToScreen(obj.Position).Y;
-                Drawing.DrawText(X, Y, Color.DarkTurquoise, (obj is Obj_AI_Hero) ? ((Obj_AI_Hero)obj).CharData.BaseSkinName : obj.Name);
-                Drawing.DrawText(X, Y + 10, Color.DarkTurquoise, obj.Type.ToString());
-                Drawing.DrawText(X, Y + 20, Color.DarkTurquoise, "NetworkID: " + obj.NetworkId);
-                Drawing.DrawText(X, Y + 30, Color.DarkTurquoise, obj.Position.ToString());
+                if (!obj.IsValid)
+                {
+                    return;
+                }
+
+                var x = Drawing.WorldToScreen(obj.Position).X;
+                var y = Drawing.WorldToScreen(obj.Position).Y;
+
+                Drawing.DrawText(x, y, Color.DarkTurquoise, "IsAttackable: " + (obj is AttackableUnit));
+                Drawing.DrawText(x, y + 10, Color.DarkTurquoise, "IsBase: " + (obj is Obj_AI_Base));
+                Drawing.DrawText(x, y + 20, Color.DarkTurquoise, "IsMinion: " + (obj is Obj_AI_Minion));
+                Drawing.DrawText(x, y + 30, Color.DarkTurquoise, "IsHero: " + (obj is Obj_AI_Hero));
+
                 if (obj is Obj_AI_Base)
                 {
                     var aiobj = obj as Obj_AI_Base;
-                    Drawing.DrawText(X, Y + 40, Color.DarkTurquoise, "Health: " + aiobj.Health + "/" + aiobj.MaxHealth + "(" + aiobj.HealthPercent + "%)");
+                    Drawing.DrawText(x, y + 40, Color.DarkTurquoise, "BaseName: " + aiobj.CharData.BaseSkinName);
+                    Drawing.DrawText(x, y + 50, Color.DarkTurquoise, "Name: " + aiobj.CharData.Name);
                 }
-                if (obj is Obj_AI_Hero)
+                else if (obj is AttackableUnit)
                 {
-                    var hero = obj as Obj_AI_Hero;
-                    Drawing.DrawText(X, Y + 50, Color.DarkTurquoise, "Spells:");
-                    Drawing.DrawText(X, Y + 60, Color.DarkTurquoise, "(Q): " + hero.Spellbook.Spells[0].Name);
-                    Drawing.DrawText(X, Y + 70, Color.DarkTurquoise, "(W): " + hero.Spellbook.Spells[1].Name);
-                    Drawing.DrawText(X, Y + 80, Color.DarkTurquoise, "(E): " + hero.Spellbook.Spells[2].Name);
-                    Drawing.DrawText(X, Y + 90, Color.DarkTurquoise, "(R): " + hero.Spellbook.Spells[3].Name);
-                    Drawing.DrawText(X, Y + 100, Color.DarkTurquoise, "(D): " + hero.Spellbook.Spells[4].Name);
-                    Drawing.DrawText(X, Y + 110, Color.DarkTurquoise, "(F): " + hero.Spellbook.Spells[5].Name);
-                    var buffs = hero.Buffs;
-                    if (buffs.Any())
-                    {
-                        Drawing.DrawText(X, Y + 120, Color.DarkTurquoise, "Buffs:");
-                    }
-                    for (var i = 0; i < buffs.Count()*10; i += 10)
-                    {
-                        Drawing.DrawText(X, (Y + 130 + i), Color.DarkTurquoise, buffs[i/10].Count + "x " + buffs[i/10].Name);
-                    }
-
-                }
-                if (obj is Obj_SpellMissile)
-                {
-                    var missile = obj as Obj_SpellMissile;
-                    Drawing.DrawText(X, Y + 40, Color.DarkTurquoise, "Missile Speed: " + missile.SData.MissileSpeed);
-                    Drawing.DrawText(X, Y + 50, Color.DarkTurquoise, "Cast Range: " + missile.SData.CastRange);
-                }
-
-                if (obj is MissileClient && obj.Name != "missile")
-                {
-                    var missile = obj as MissileClient;
-                    Drawing.DrawText(X, Y + 40, Color.DarkTurquoise, "Missile Speed: " + missile.SData.MissileSpeed);
-                    Drawing.DrawText(X, Y + 50, Color.DarkTurquoise, "Cast Range: " + missile.SData.CastRange);
+                    var unit = obj as AttackableUnit;
+                    Drawing.DrawText(x, y + 40, Color.DarkTurquoise, "Name: " + unit.Name);
                 }
             }
         }
